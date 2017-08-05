b57933760f0cd674231bd18219c3babd4ade1906
Ayan George <ayan@ayan.net>
2017-08-05 10:57:52 -0400

    Add code to render templates in assets/templates/

--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 )
+
+type TemplateRenderer interface {
+	Render(io.Writer, string, interface{}) error
+}
+
+type StaticTemplate struct {
+	Glob string
+}
+
+type DebugTemplate struct {
+	Glob string
+}
+
+func (d DebugTemplate) Render(w io.Writer, name string, data interface{}) error {
+	t := template.Must(template.ParseGlob(d.Glob))
+	return t.ExecuteTemplate(w, name, data)
+}
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html></html><body><h1>Hello!</h1></body></html>")
@@ -39,7 +58,16 @@ func main() {
 
 	log.Printf("Lisening on %q type socket at %q.", *network, *address)
 
-	http.HandleFunc("/", RootHandler)
+	templater := DebugTemplate{"assets/templates/*.html"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { templater.Render(w, "index.html", nil) })
+
+	http.HandleFunc("/meetings/", func(w http.ResponseWriter, r *http.Request) { templater.Render(w, "meetings.html", nil) })
+	http.HandleFunc("/jobs/", func(w http.ResponseWriter, r *http.Request) { templater.Render(w, "jobs.html", nil) })
+	http.HandleFunc("/links/", func(w http.ResponseWriter, r *http.Request) { templater.Render(w, "links.html", nil) })
+
+	http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("assets/images"))))
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("assets/css"))))
 
 	go http.Serve(l, nil)
 
