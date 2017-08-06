c26e9b57e0682ffe44d4dc0feffeafce3edf14f1
Ayan George <ayan@boltcarrier.com>
2017-08-06 16:00:41 +0000

    Add move SQL connection related code to PostgressConnect()

--- main.go
+++ main.go
@@ -12,6 +12,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func PostgresConnect(user, host, database string) (*sql.DB, error) {
+	connstring := fmt.Sprintf("password=\"\" user=%s host=%s dbname=%s sslmode=disable", user, host, database)
+
+	db, err := sql.Open("postgres", connstring)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	default_dbhost := "localhost"
 	switch runtime.GOOS {
@@ -24,31 +38,19 @@ func main() {
 	dbname := flag.String("dbname", "orlandogolang", "Name of database to use.")
 	dbuser := flag.String("dbuser", "ayan", "Database user to connect as.")
 	dbhost := flag.String("dbhost", default_dbhost, "Unix-domain socket path or hostname of db server to use.")
-
 	network := flag.String("net", "tcp", "Network to listen on.  Should be either \"tcp\" or \"unix\"")
 	address := flag.String("addr", "localhost:9898", "Address to listen on.  This should be apropriate to the network chosen.")
 
 	flag.Parse()
 
-	// FIXME: this should be a named function.
-	db, err := func(database string, user string, host string) (*sql.DB, error) {
-		connstring := fmt.Sprintf("password=\"\" user=%s host=%s dbname=%s sslmode=disable", user, host, database)
-		db, err := sql.Open("postgres", connstring)
-		if err != nil {
-			return nil, err
-		}
-
-		if err := db.Ping(); err != nil {
-			return nil, err
-		}
-
-		return db, nil
-	}(*dbname, *dbuser, *dbhost)
+	db, err := PostgresConnect(*dbname, *dbuser, *dbhost)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// FIXME: i should probably wait until UpdateMeetupDatabase() is complete.
+	// We shouldn't quit mid-query.
 	go Webserver(*network, *address, db)
 	go UpdateMeetupDatabase(db)
 
